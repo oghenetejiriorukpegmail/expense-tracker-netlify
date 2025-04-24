66df087104f593668e9fe32c0fdcf5b1e1456340
oghenetejiriorukpegmail <oghenetejiriorukpe@gmail.com>
2025-04-24 18:29:37 -0400

    Fix dark mode rendering by updating theme.json and adding CSS variables

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./theme.css"; // Import the theme CSS
 import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your Publishable Key
